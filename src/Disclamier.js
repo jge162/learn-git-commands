e0e223f5ef9d0547b3b44d0a666d6af47096fb23
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-05-17 08:14:16 +0000

    changed icon again

--- src/Disclamier.js
+++ src/Disclamier.js
@@ -4,7 +4,7 @@ const Disclaimer = () => {
   return (
     <div className="disclaimer">
       <p>
-        Disclaimer: The definitions and practical uses provided on this website are based on personal understanding and should not be considered official or authoritative. This information is intended for educational purposes only and does not constitute professional advice. Use the content at your own discretion.
+        <strong>Disclaimer:</strong> The definitions and practical uses provided on this website are based on personal understanding and should not be considered official or authoritative. This information is intended for educational purposes only and does not constitute professional advice. Use the content at your own discretion.
       </p>
     </div>
   );
