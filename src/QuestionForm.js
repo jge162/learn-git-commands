9487ffd9352a0f02d781030c5fcd7d5fd4dec2e7
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-06-14 00:07:33 -0700

    Update QuestionForm.js

--- src/QuestionForm.js
+++ src/QuestionForm.js
@@ -86,7 +86,7 @@ const QuestionForm = () => {
     </div>
        <Footer />
       <div className="icons">
-        <a href="https://www.linkedin.com/in/jeremy-escobar/" className="icon-container" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/jeremyescobar/" className="icon-container" target="_blank" rel="noopener noreferrer">
           <LinkedInIcon className="icon" style={{ fontSize: 60 }} />
         </a>
         <a href="https://github.com/jge162" className="icon-container" target="_blank" rel="noopener noreferrer">
